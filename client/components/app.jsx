0f25fcb9bacf004b57e4d3fc632d1e1458cd925f
kenneth wang <kennethwang0427@gmail.com>
2021-06-07 08:57:14 -0700

    update

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,76 +2,93 @@ import React from 'react';
 import axios from 'axios';
 import Overview from './Overview/Overview.jsx';
 import RelatedItems from './RelatedItems/RelatedItems.jsx';
-import QA from './QA/QA.jsx';
 import Reviews from './Reviews/Reviews.jsx';
+import QA from './QA/QA.jsx';
+
+
 
 class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
       list:[],
-      targetId: 25711,//reveiws testing. we can initialize with a particular ID
-      styles: []
+      targetId: 25821,//reveiws testing. we can initialize with a particular ID
+      styles: [],
+      loaded: false
     };
-    this.fetchEverything = this.fetchEverything.bind(this);
     this.fetchGET = this.fetchGET.bind(this);
-    this.getStyles = this.getStyles.bind(this);
+    this.renderPage = this.renderPage.bind(this);
+    this.fetchEverything = this.fetchEverything.bind(this);
+    this.testing = this.testing.bind(this);
 
-  };
+  }
 
   componentDidMount(){
     this.fetchEverything();
-  };
-
-  fetchGET(string, id){
-    axios.get('/get', {
-      params: {
-        endpoint:`${string}/${id}`
-      }},)
-      .then((data) =>{
-        this.setState({
-          [string]: response.data,
-          //has to set state for data.[whatever key we need from data]
-        })
-      })
-      .catch(err=>{
-        console.log(err)
-      });
-  };
+  }
 
 
-  //refactor later
-  getStyles(string, id){
-    axios.get('/get', {params: {endpoint: `${string}/${id}`}})
+  fetchGET(string, endpoint, stateName){
+    return (
+      axios.get('/get', {params: {endpoint: `${string}/${endpoint}`}})
       .then((response) =>{
-        console.log('successful get request', `${string}/${id}`);
+        console.log('successful get request', `${string}/${endpoint}`);
         this.setState({
-          styles: response.data,
-        })
+          [stateName]: response.data,
+          //has to set state for data.[whatever key we need from data]
+        }, () =>this.setState({loaded: true}))
       })
-      .catch(err=>{
-        console.log(err)
-      });
+      .catch(err=> console.error(err))
+    );
   };
 
   fetchEverything() {
-    this.fetchGET('products', this.state.targetId);
-    this.getStyles('products', `${this.state.targetId}/styles`);
-    //await this.fetchGET('relatedItems');
-    //await this.fetchGET('QA');
-    //await this.fetchGET('reviews');
+
+    this.fetchGET('qa', `questions/?product_id=${this.state.targetId}`, 'questions');
+    //this.fetchGET('products', this.state.targetId, 'list');
+  }
+
+  testing(){
+    if(this.state.targetId === 25821){
+      this.setState({
+        targetId:25711,
+      },() =>{
+        this.fetchGET('qa', `questions/?product_id=${this.state.targetId}`, 'questions');
+      })
+    }else{
+      this.setState({
+        targetId:25821,
+      },() =>{
+        this.fetchGET('qa', `questions/?product_id=${this.state.targetId}`, 'questions');
+      })
+    }
   };
 
-  componentDidMount(){
-    this.fetchEverything();
-  };
+  renderPage() {
+    if(this.state.loaded) {
+      return (
+        <div>
+          <Overview id = {this.state.targetId}/>
+          <RelatedItems id={this.state.targetId} fetchGET={this.fetchGET} />
+          <QA questions={this.state.questions}/>
+          <Reviews id ={this.state.targetId}/>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          Page Loading ...
+        </div>
+      )
+    }
+  }
 
   render(){
     return (
       <div>
-         <Overview info = {this.state.list} callback = {this.productInfo} styles = {this.state.styles}/>
-         <Reviews id ={this.state.targetId}/>
-
+        {/* {this.renderPage()} */}
+        <button onClick = {this.testing}> TESTING </button>
+        <Reviews id ={this.state.targetId}/>
       </div>
     )
   }
@@ -79,13 +96,3 @@ class App extends React.Component{
 
 export default App;
 
-
-/*
-put the individual components here. they are not exported yet so throwing an error
-<RelatedItems />
-<QA />
-<Reviews />
-
-
-
-*/
